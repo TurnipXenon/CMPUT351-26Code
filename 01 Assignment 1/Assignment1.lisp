f477a2a621a8a986362f020b051b3c9fd805198e
Allan Manuba <manuba@ualberta.ca>
2021-01-26 13:43:06 -0700

    A1 Q4

--- 01 Assignment 1/Assignment1.lisp
+++ 01 Assignment 1/Assignment1.lisp
@@ -1,22 +1,37 @@
 ;QUESTION 1
 ;todo: documentation
 (defun xmember (X Y)
-    (cond   ((null Y) nil)
-            ((atom Y) nil)
-            (t (or (or  (equal X (car Y))
-                        (xmember X (car Y))
-                    )
-                    (xmember X (cdr Y))
-                )
+    (if (or (null Y) (atom Y))
+        nil
+        (or (or (equal X (car Y))
+                (xmember X (car Y))
             )
+            (xmember X (cdr Y))
+        )
     )
 )
+
+;; (xmember '1 '(1))
+;; T
+;; >(xmember '1 '( (1) 2 3))
+;; NIL
+;; >(xmember '(1) '((1) 2 3))
+;; T
+;; (xmember nil nil)
+;; NIL
+;; (xmember nil '(nil))
+;; T
+;; (xmember nil '((nil)))
+;; NIL
+;; (xmember '(nil) '(1 2 3 (nil)))
+;; T
+(xmember '(nil) '(nil))
 
 ;QUESTION 2
 ;todo: documentation
 (defun flatten (X)
     (cond
-        ((null X) X)
+        ((null X) nil)
         ((atom (car X)) 
                 (cons (car X)
                     (flatten (cdr X))))
@@ -32,3 +47,21 @@
 ;; (flatten '(a (b c) d))
 ;; (flatten '((((a)))))
 ;; (flatten '(a (b c) (d ((e)) f)))
+
+;QUESTION 3
+;todo: documentation
+(defun mix_helper (L1 L2 AC)
+    (cond
+        ((null L1) (append AC L2))
+        ((null L2) (append AC L1))
+        (t (append
+                (cons (car L1) (cons(car L2) nil))
+                (mix_helper (cdr L1) (cdr L2) AC)
+            )
+        )
+    )
+)
+
+(defun mix (L1 L2)
+    (mix_helper L1 L2 nil)
+)
