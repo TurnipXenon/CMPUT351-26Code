197a7b87b3e1ecb3d0b70d5d3594e84d4190c549
Allan Manuba <manuba@ualberta.ca>
2021-01-26 13:23:24 -0700

    A1 Q2

--- 01 Assignment 1/Assignment1.lisp
+++ 01 Assignment 1/Assignment1.lisp
@@ -1,24 +1,34 @@
 ;QUESTION 1
 ;todo: documentation
-;; (defun xmember (X Y)
-;;     (if (null Y)
-;;         nil
-;;         (or (or (equal X (car Y))
-;;                 (xmember)
-;;                 )
-;;             ()
-;;             )
-;;     )
-;; )
-
 (defun xmember (X Y)
     (cond   ((null Y) nil)
             ((atom Y) nil)
-            (t (or (or (equal X (car Y))
-                    (xmember X (car Y))
-                        )
-                    (xmember X (cdr Y))
+            (t (or (or  (equal X (car Y))
+                        (xmember X (car Y))
                     )
+                    (xmember X (cdr Y))
                 )
+            )
     )
 )
+
+;QUESTION 2
+;todo: documentation
+(defun flatten (X)
+    (cond
+        ((null X) X)
+        ((atom (car X)) 
+                (cons (car X)
+                    (flatten (cdr X))))
+        (t
+            (append
+                (flatten (car X))
+                (flatten (cdr X))
+            )
+        )
+    )
+)
+
+;; (flatten '(a (b c) d))
+;; (flatten '((((a)))))
+;; (flatten '(a (b c) (d ((e)) f)))
