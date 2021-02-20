462b29c2782386b6315bb373485e9aee7c99d4a5
Allan Manuba <allanmanuba@gmail.com>
2021-02-20 01:33:36 -0700

    Work for one user-defined function

--- 02Assignment2/1579003.lisp
+++ 02Assignment2/1579003.lisp
@@ -1,3 +1,115 @@
+; todo: document
+; assume F is an atom
+#| Helper function count-elements
+
+The funtion count-elements returns a number which represents the number of elements
+in the list L.
+
+This is a helper function for find-fun.
+
+Test cases:
+> (count-elements nil) => 0
+> (count-elements '((a))) => 1
+> (count-elements '(() a)) => 2
+
+|#
+(defun count-elements (L)
+  (if (null L)
+    0
+    (+ 1 (count-elements (cdr L)))
+  )
+)
+
+; assumes a valid program
+(defun find-fun (F P)
+  (cond
+    ((null P) nil)
+    (
+      (and
+        (eq (car F) (caar P)) ; get the first item in the first pair
+        (eq (count-elements (cdr F)) (count-elements (second (car P)))) ; check for equal arity
+      )
+      (car P)
+    )
+    (t (find-fun F (cdr P)))
+  )
+)
+
+; todo: document
+; todo: list interpreter
+#| Helper function interp-list
+
+The funtion count-elements returns a number which represents the number of elements
+in the list L.
+
+This is a helper function for find-fun.
+
+Test cases:
+> (count-elements nil) => 0
+> (count-elements '((a))) => 1
+> (count-elements '(() a)) => 2
+
+|#
+
+
+
+
+#| Helper function fl-lambda
+
+The funtion count-elements returns a number which represents the number of elements
+in the list L.
+
+This is a helper function for find-fun.
+
+Test cases:
+> (replace0 '(X Y) '(1 2) 'Z) > Z
+> (replace0 '(X Y) '(1 2) 'Y) > 2
+
+|#
+
+;; (defun replace (A V E)
+;;   A
+;; )
+
+; assume len(A) == len(V)
+(defun replace0 (A V X)
+  (cond
+    ((null A) X)
+    ((eq (car A) X) (car V))
+    (t (replace0 (cdr A) (cdr V) X))
+  )
+)
+
+#| Helper function fl-lambda
+
+The funtion count-elements returns a number which represents the number of elements
+in the list L.
+
+This is a helper function for find-fun.
+
+Test cases:
+> (fl-sub (X Y) (3 5) (if (> x y) x (if (< x y) y nil))) > (if (> 3 5) 3 (if (< 3 5) 5 nil))
+
+|#
+
+(defun fl-sub (A V E)
+  (if (null E)
+    nil
+    (cons
+      (if (atom (car E))
+        (replace0 A V (car E))
+        (fl-sub A V (car E))
+      )
+      (fl-sub A V (cdr E)) ; move to next element but also replace deep
+    )
+  )
+)
+
+;; (defun fl-sub (A V E)
+;;   E
+;; )
+
+; todo: document
 (defun fl-interp (E P)
   (cond
     (
@@ -130,9 +242,27 @@
           ; otherwise f is defined (not intended to be a function),
           ; the E is returned as if it quoted in lisp
           ; todo: error handling
+          (
+            (find-fun E P)
+            ;; t
+            ; reference: https://stackoverflow.com/a/5959697/10024566
+            (eval 
+              (fl-sub
+                (cadar P)
+                (mapcar (lambda (x) (fl-interp x P)) (cdr E))
+                (third (cdar P))
+              )
+            )
+            ;; (fl-interp (cons '> (cdr E)) P)
+          )
+
           (t E)
         )
       )
     )
   )
 )
+
+;; (fl-interp (cons '> (cdr '(greater 3 5))) nil)
+;; (mapcar 'fl-interp '((1 nil) (2 nil) (3 nil)))
+;; (mapcar (lambda (x) (fl-interp x nil)) '(1 2 3))
--- 02Assignment2/Assignment2Tests.lisp
+++ 02Assignment2/Assignment2Tests.lisp
@@ -1,24 +1,69 @@
-(set 't1 '(fl-interp '(+ 10 5) nil)) ; > '15
-(set 't2 '(fl-interp '(- 12 8) nil)) ; > '4
-(set 't3 '(fl-interp '(* 5 9) nil))
-(set 't4 '(fl-interp '(> 2 3) nil))
-(set 't5 '(fl-interp '(< 1 131) nil))
-(set 't6 '(fl-interp '(= 88 88) nil))
-(set 't7 '(fl-interp '(and false true) nil))
-(set 't8 '(fl-interp '(or true false) nil))
-(set 't9 '(fl-interp '(not true) nil))
-(set 't10 '(fl-interp '(number 354) nil))
-(set 't11 '(fl-interp '(equal (3 4 1) (3 4 1)) nil))
-(set 't12 '(fl-interp '(if false 2 3) nil))
-(set 't13 '(fl-interp '(null ()) nil))
-(set 't14 '(fl-interp '(atom (3)) nil))
-(set 't15 '(fl-interp '(eq x x) nil))
-(set 't16 '(fl-interp '(first (8 5 16)) nil))
-(set 't17 '(fl-interp '(rest (8 5 16)) nil))
-(set 't18 '(fl-interp '(cons 6 3) nil))
-(set 't19 '(fl-interp '(+ (* 2 2) (* 2 (- (+ 2 (+ 1 (- 7 4))) 2))) nil))
-(set 't20 '(fl-interp '(and (> (+ 3 2) (- 4 2)) (or (< 3 (* 2 2))) (not (= 3 2))) nil))
-(set 't21 '(fl-interp '(or (= 5 (- 4 2)) (and (not (> 2 2)) (< 3 2))) nil))
-(set 't22 '(fl-interp '(if (not (null (first (a c e)))) (if (number (first (a c e))) (first (a c e)) (cons (a c e) d)) (rest (a c e))) nil))
+(set 'pt1 '(fl-interp '(+ 10 5) nil)) ; > '15
+(set 'pt2 '(fl-interp '(- 12 8) nil)) ; > '4
+(set 'pt3 '(fl-interp '(* 5 9) nil))
+(set 'pt4 '(fl-interp '(> 2 3) nil))
+(set 'pt5 '(fl-interp '(< 1 131) nil))
+(set 'pt6 '(fl-interp '(= 88 88) nil))
+(set 'pt7 '(fl-interp '(and false true) nil))
+(set 'pt8 '(fl-interp '(or true false) nil))
+(set 'pt9 '(fl-interp '(not true) nil))
+(set 'pt10 '(fl-interp '(number 354) nil))
+(set 'pt11 '(fl-interp '(equal (3 4 1) (3 4 1)) nil))
+(set 'pt12 '(fl-interp '(if false 2 3) nil))
+(set 'pt13 '(fl-interp '(null ()) nil))
+(set 'pt14 '(fl-interp '(atom (3)) nil))
+(set 'pt15 '(fl-interp '(eq x x) nil))
+(set 'pt16 '(fl-interp '(first (8 5 16)) nil))
+(set 'pt17 '(fl-interp '(rest (8 5 16)) nil))
+(set 'pt18 '(fl-interp '(cons 6 3) nil))
+(set 'pt19 '(fl-interp '(+ (* 2 2) (* 2 (- (+ 2 (+ 1 (- 7 4))) 2))) nil))
+(set 'pt20 '(fl-interp '(and (> (+ 3 2) (- 4 2)) (or (< 3 (* 2 2))) (not (= 3 2))) nil))
+(set 'pt21 '(fl-interp '(or (= 5 (- 4 2)) (and (not (> 2 2)) (< 3 2))) nil))
+(set 'pt22 '(fl-interp '(if (not (null (first (a c e)))) (if (number (first (a c e))) (first (a c e)) (cons (a c e) d)) (rest (a c e))) nil))
 
-(eval t1)
+; Test find-fun
+(set 'fft1 '(find-fun '(greater 3 5) '((greater (x y) = (if (> x y) x (if (< x y) y nil)))))) ; > t
+(set 'fft2 '(find-fun '(greater 3 5) '((lesser (x y) = (if (> x y) x (if (< x y) y nil)))))) ; > nil
+(set 'fft3 '(find-fun '(greater 3 5) '((greater (x y z) = (if (> x y) x (if (< x y) y nil)))))) ; > nil
+
+; Test count-elements
+(set 'cet1 '(count-elements nil)) ; > 0
+(set 'cet2 '(count-elements '((a)))) ; > 1
+(set 'cet3 '(count-elements '(() a))) ; > 2
+
+;
+;; (set 'rt1 '(replace0 'X 3 '(if (> x y) x (if (< x y) y nil)))) ; > (if (> 3 Y) 3 (if (< 3 Y) Y nil))
+
+; FL-Sub test
+(set 'fst1 '(fl-sub '(X Y) '(3 5) '(if (> x y) x (if (< x y) y nil)))) ; > (if (> 3 5) 3 (if (< 3 5) 5 nil))
+
+
+; (greater (x y) = (if (> x y) x (if (< x y) y nil)))
+; User-defined
+(set 'ut1 '(fl-interp '(greater 3 5) '((greater (x y) = (if (> x y) x (if (< x y) y nil)))))) ; > '5
+
+;; (lambda (x) (if (> x y) x (if (< x y) y nil)))
+;; (lambda (y) (lambda (x) (if (> x y) x (if (< x y) y nil))))
+;; (((lambda (x) (lambda (y) (if (> x y) x (if (< x y) y nil)))) 3) 5)
+;; (((lambda (x)
+;;     (lambda (y) (if (< x y) x y))
+;; )5)6)
+
+;; ((
+;;     lambda (x)
+;;     (lambda (y) (if (< x y) x y))
+;; ) 5 6)
+
+;; (funcall
+;;     (funcall
+;;         (function
+;;             (lambda (x)
+;;                 (function 
+;;                     (lambda (y)
+;;                         (if (< x y) x y)
+;;                     )
+;;                 )
+;;             )
+;;         )
+;;     4)
+;; 9)
