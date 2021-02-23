a2daa661c2545545aa2a3a6cdd6703f9f0e14cb1
Allan Manuba <allanmanuba@gmail.com>
2021-02-22 20:25:26 -0700

    Refactor, test, and document assignment 2

--- 02Assignment2/1579003.lisp
+++ 02Assignment2/1579003.lisp
@@ -1,18 +1,17 @@
-; todo: document
-; assume F is an atom
 #| Helper function count-elements
 
-The funtion count-elements returns a number which represents the number of elements
+The function count-elements returns a number which represents the number of elements
 in the list L.
 
 This is a helper function for find-fun.
 
 Test cases:
-> (count-elements nil) => 0
-> (count-elements '((a))) => 1
-> (count-elements '(() a)) => 2
+> (count-elements nil) ; > 0
+> (count-elements '((a))) ; > 1
+> (count-elements '(() a)) ; > 2
 
 |#
+
 (defun count-elements (L)
   (if (null L)
     0
@@ -20,7 +19,27 @@ Test cases:
   )
 )
 
-; assumes a valid program
+
+
+#| Helper function find-fun
+
+Given a function F, and a program P (which is a list of function 
+definitions), the function find-fun returns a matching function definition if said
+function definition exists in P. Otherwise, nil is returned. A function definition
+matches with a function if its name and arity matches with the funciton definition's
+header.
+
+This is a helper function for find-fun.
+
+This assumes that F has a proper format for a function, and P is a valid program.
+
+Test cases:
+(find-fun '(greater 3 5) '((greater (x y) = (if (> x y) x (if (< x y) y nil))))) ; > t
+(find-fun '(greater 3 5) '((lesser (x y) = (if (> x y) x (if (< x y) y nil))))) ; > nil
+(find-fun '(greater 3 5) '((greater (x y z) = (if (> x y) x (if (< x y) y nil))))) ; > nil
+
+|#
+
 (defun find-fun (F P)
   (cond
     ((null P) nil)
@@ -35,96 +54,92 @@ Test cases:
   )
 )
 
-; todo: document
-; todo: list interpreter
-#| Helper function interp-list
-
-The funtion count-elements returns a number which represents the number of elements
-in the list L.
-
-This is a helper function for find-fun.
-
-Test cases:
-> (count-elements nil) => 0
-> (count-elements '((a))) => 1
-> (count-elements '(() a)) => 2
-
-|#
 
 
+#| Helper function replace0
+Given a list of variables A and a list of values V, the function returns a matching value in V
+if X is equal to a variable in A. Otherwise, X is returned. A variable in A matches with 
+a value in V if they both are in the same position in their list. This assumes that 
+the length of A and length of V are equal.
 
-
-#| Helper function fl-lambda
-
-The funtion count-elements returns a number which represents the number of elements
-in the list L.
-
-This is a helper function for find-fun.
+This is a helper function for fl-sub.
 
 Test cases:
 > (replace0 '(X Y) '(1 2) 'Z) > Z
 > (replace0 '(X Y) '(1 2) 'Y) > 2
 
 |#
-
-(defun datafy (X)
-  (cons 'quote (cons X nil))
-)
-
-;; (defun replace (A V E)
-;;   A
-;; )
-
-; assume len(A) == len(V)
 (defun replace0 (A V X)
   (cond
     ((null A) X)
     (
       (eq (car A) X) 
       (car V)
-      ;; (cons 'quote (cons (car V) nil))
     )
     (t (replace0 (cdr A) (cdr V) X))
   )
 )
 
-#| Helper function fl-lambda
 
-The funtion count-elements returns a number which represents the number of elements
-in the list L.
 
-This is a helper function for find-fun.
+#| Helper function fl-sub
+
+Given the list of variables A, list of values V, and function body B, the function returns
+an expression based on B where all the variables are substituted with a matching value.
+A variable in A matches with a value in V if they both are in the same position in their list. 
+This assumes that the length of A and length of V are equal.
+
+This is a helper function for fl-interp for substituting a matching function header
+with an appropriate function body.
 
 Test cases:
-> (fl-sub (X Y) (3 5) (if (> x y) x (if (< x y) y nil))) > (if (> 3 5) 3 (if (< 3 5) 5 nil))
-> (fl-sub )
+> '(fl-sub '(X Y) '(3 5) '(if (> x y) x (if (< x y) y nil))) ; > (if (> 3 5) 3 (if (< 3 5) 5 nil))
+> '(fl-sub '(A B) '(3 5) '(if (> x y) x (if (< x y) y nil))) ; > (if (> x y) x (if (< x y) y nil))
 |#
 
-(defun fl-sub (A V E)
+(defun fl-sub (A V B)
   (cond 
-    ((null E) nil)
+    ((null B) nil)
     (
-      (atom E)
-      (replace0 A V E)
+      (atom B)
+      (replace0 A V B)
     )
     (
       t
       (cons
-        (if (atom (car E))
-          (replace0 A V (car E))
-          (fl-sub A V (car E))
+        (if (atom (car B))
+          (replace0 A V (car B)) ; replace the atom
+          (fl-sub A V (car B)) ; replace deeply if list
         )
-        (fl-sub A V (cdr E)) ; move to next element but also replace deep
+        (fl-sub A V (cdr B)) ; move to next element
       )
     )
   )
 )
 
-;; (defun fl-sub (A V E)
-;;   E
-;; )
 
-; todo: document
+
+#| fl-interp
+
+Taken from the assignment specification:
+
+A program P in FL is a list of function definitions. The FL interpreter takes such a program, 
+together with a function application, and returns the result of evaluating the application. 
+This evaluation is based on the principle of "replacing equals by equals". Your interpreter 
+should be defined as a Lisp function
+
+(fl-interp E P)
+
+which, given a program P and an expression E, returns the result of evaluating E with 
+respect to P.
+
+Test cases:
+> (fl-interp '(+ 10 5) nil) ; > '15
+> (fl-interp '(+ (* 2 2) (* 2 (- (+ 2 (+ 1 (- 7 4))) 2))) nil) ; > '12
+> (fl-interp '(cadr (5 1 2 7)) '((cadr(x) = (first (rest x))))) ; > '1
+> (fl-interp '(divide 24 4) '((divide (x y) = (div x y 0)) (div (x y z) = (if (> (* y z) x) (- z 1) (div x y (+ z 1)))))) ; > '6
+|#
+
 (defun fl-interp (E P)
   (cond
     (
@@ -136,12 +151,13 @@ Test cases:
       t
     )
     (
+      ; this includes the case where E is nil or a number
       (atom E) E
-    ) ; this includes the case  where E is nil or a number
+    )
     (t
       (let ((f (car E)) (arg (cdr E)))
         (cond
-          ; handle built-in functions
+          ; handle primitive functions
           (
             (eq f 'if) 
             (if 
@@ -252,19 +268,17 @@ Test cases:
             (not (fl-interp (car arg) P))
           )
           ; if f is a user-defined function, then evaluate arguments 
-          ;(applicative order reduction)
-          ; todo: user-defined functions
-          ; otherwise f is defined (not intended to be a function),
-          ; the E is returned as if it quoted in lisp
-          ; todo: error handling
+          ; (applicative order reduction)
           (
             (find-fun E P)
-            ; reference: https://stackoverflow.com/a/5959697/10024566
             (let
-              ((FE (find-fun E P))) ; function expression???
+              ((FE (find-fun E P)))
               (fl-interp 
                 (fl-sub
                   (second FE)
+                  ; Reference: 
+                  ; Terje Norderhaug. Multiple arguments to mapcar. 
+                  ; https://stackoverflow.com/a/5959697/10024566
                   (mapcar (lambda (x) (fl-interp x P)) (cdr E))
                   (fourth FE)
                 )
@@ -272,22 +286,11 @@ Test cases:
               )
             )
           )
-
+          ; otherwise f is defined (not intended to be a function),
+          ; then E is returned as if it is quoted in lisp
           (t E)
         )
       )
     )
-  )
-)
-
-;; (fl-interp (cons '> (cdr '(greater 3 5))) nil)
-;; (mapcar 'fl-interp '((1 nil) (2 nil) (3 nil)))
-;; (mapcar (lambda (x) (fl-interp x nil)) '(1 2 3))
-
-(defun last0 (X)
-  (if
-    (null (rest X))
-    (first X)
-    (last0 (rest X))
   )
 )
--- 02Assignment2/Assignment2Tests.lisp
+++ 02Assignment2/Assignment2Tests.lisp
@@ -1,25 +1,25 @@
 (set 'pt1 '(fl-interp '(+ 10 5) nil)) ; > '15
 (set 'pt2 '(fl-interp '(- 12 8) nil)) ; > '4
-(set 'pt3 '(fl-interp '(* 5 9) nil))
-(set 'pt4 '(fl-interp '(> 2 3) nil))
-(set 'pt5 '(fl-interp '(< 1 131) nil))
-(set 'pt6 '(fl-interp '(= 88 88) nil))
-(set 'pt7 '(fl-interp '(and false true) nil))
-(set 'pt8 '(fl-interp '(or true false) nil))
-(set 'pt9 '(fl-interp '(not true) nil))
-(set 'pt10 '(fl-interp '(number 354) nil))
-(set 'pt11 '(fl-interp '(equal (3 4 1) (3 4 1)) nil))
-(set 'pt12 '(fl-interp '(if false 2 3) nil))
-(set 'pt13 '(fl-interp '(null ()) nil))
-(set 'pt14 '(fl-interp '(atom (3)) nil))
-(set 'pt15 '(fl-interp '(eq x x) nil))
-(set 'pt16 '(fl-interp '(first (8 5 16)) nil))
-(set 'pt17 '(fl-interp '(rest (8 5 16)) nil))
-(set 'pt18 '(fl-interp '(cons 6 3) nil))
-(set 'pt19 '(fl-interp '(+ (* 2 2) (* 2 (- (+ 2 (+ 1 (- 7 4))) 2))) nil))
-(set 'pt20 '(fl-interp '(and (> (+ 3 2) (- 4 2)) (or (< 3 (* 2 2))) (not (= 3 2))) nil))
-(set 'pt21 '(fl-interp '(or (= 5 (- 4 2)) (and (not (> 2 2)) (< 3 2))) nil))
-(set 'pt22 '(fl-interp '(if (not (null (first (a c e)))) (if (number (first (a c e))) (first (a c e)) (cons (a c e) d)) (rest (a c e))) nil))
+(set 'pt3 '(fl-interp '(* 5 9) nil)) ; > 45
+(set 'pt4 '(fl-interp '(> 2 3) nil)) ; > 'nil
+(set 'pt5 '(fl-interp '(< 1 131) nil)) ; > 't
+(set 'pt6 '(fl-interp '(= 88 88) nil)) ; > 't
+(set 'pt7 '(fl-interp '(and false true) nil)) ; > 'nil
+(set 'pt8 '(fl-interp '(or true false) nil)) ; >'t
+(set 'pt9 '(fl-interp '(not true) nil)) ; > 'nil
+(set 'pt10 '(fl-interp '(number 354) nil)) ; > 't
+(set 'pt11 '(fl-interp '(equal (3 4 1) (3 4 1)) nil)) ; >'t
+(set 'pt12 '(fl-interp '(if false 2 3) nil)) ; >'3
+(set 'pt13 '(fl-interp '(null ()) nil)) ; >'t
+(set 'pt14 '(fl-interp '(atom (3)) nil)) ; >'nil
+(set 'pt15 '(fl-interp '(eq x x) nil)) ; > 't
+(set 'pt16 '(fl-interp '(first (8 5 16)) nil)) ; > '8
+(set 'pt17 '(fl-interp '(rest (8 5 16)) nil)) ; > '(5 16)
+(set 'pt18 '(fl-interp '(cons 6 3) nil)) ; >'(6 . 3)
+(set 'pt19 '(fl-interp '(+ (* 2 2) (* 2 (- (+ 2 (+ 1 (- 7 4))) 2))) nil)) ; > '12
+(set 'pt20 '(fl-interp '(and (> (+ 3 2) (- 4 2)) (or (< 3 (* 2 2))) (not (= 3 2))) nil)) ; > 't
+(set 'pt21 '(fl-interp '(or (= 5 (- 4 2)) (and (not (> 2 2)) (< 3 2))) nil)) ; > 'nil
+(set 'pt22 '(fl-interp '(if (not (null (first (a c e)))) (if (number (first (a c e))) (first (a c e)) (cons (a c e) d)) (rest (a c e))) nil)) ; >' ((a c e) . d)
 
 ; Test find-fun
 (set 'fft1 '(find-fun '(greater 3 5) '((greater (x y) = (if (> x y) x (if (< x y) y nil)))))) ; > t
@@ -32,7 +32,7 @@
 (set 'cet3 '(count-elements '(() a))) ; > 2
 
 ;
-;; (set 'rt1 '(replace0 'X 3 '(if (> x y) x (if (< x y) y nil)))) ; > (if (> 3 Y) 3 (if (< 3 Y) Y nil))
+(set 'rt1 '(replace0 '(X Y) '(3 5) 'X)) ; > (if (> 3 Y) 3 (if (< 3 Y) Y nil))
 
 ; FL-Sub test
 (set 'fst1 '(fl-sub '(X Y) '(3 5) '(if (> x y) x (if (< x y) y nil)))) ; > (if (> 3 5) 3 (if (< 3 5) 5 nil))
@@ -53,7 +53,7 @@
 (set 'ut9 '(fl-interp '(power 4 2) '((power(x y) = (if (= y 1) x (power (* x x) (- y 1))))))) ; > '16
 (set 'ut10 '(fl-interp '(factorial 4) '((factorial(x) = (if (= x 1) 1 (* x (factorial (- x 1)))))))) ; > '24
 (set 'ut11 '(fl-interp '(divide 24 4) '((divide (x y) = (div x y 0)) (div (x y z) = (if (> (* y z) x) (- z 1) (div x y (+ z 1))))))) ; > '6
-(set 'ut12 '(fl-interp '(divide 24 4) '((divide (x y) = (divine x y 0)) (div (x y z) = (if (> (* y z) x) (- z 1) (div x y (+ z 1))))))) ; > '6
+(set 'ut12 '(fl-interp '(divide 24 4) '((divide (x y) = (divine x y 0)) (div (x y z) = (if (> (* y z) x) (- z 1) (div x y (+ z 1))))))) ; > 'divine 24 0
 
 ; self testing
 (set 'st1 '(fl-interp '(f0 1 2) '((f0 () = 0) (f0 (A) = 1) (f0 (A B) = 2) (f0 (A B C) = 3))))
