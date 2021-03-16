b85239a0765440ee71ff73b8f12dedca6afc98dd
Allan Manuba <manuba@ualberta.ca>
2021-03-15 23:50:24 -0600

    Finish part 1

--- 04Assignment3/1579003.pl
+++ 04Assignment3/1579003.pl
@@ -1,5 +1,10 @@
-% setIntersect([], _, []).
-% setIntersect(L, R, S) :- .
+isIn(_, []) :- false.
+isIn(A, [A|_]) :- true.
+isIn(A, [_|L]) :- isIn(A, L).
+
+setIntersect([], _, []).
+setIntersect([A|L], R, [A|S]) :- isIn(A,R), setIntersect(L, R, S).
+setIntersect([_|L], R, S) :- setIntersect(L, R, S).
 
 swap([], []).
 swap([A], [A]).
