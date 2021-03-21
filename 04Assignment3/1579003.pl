a21908b2b229a94f09dd5046203d56b927385adb
Allan Manuba <manuba@ualberta.ca>
2021-03-20 22:42:53 -0600

    Finish a3 q5 q6 q7

--- 04Assignment3/1579003.pl
+++ 04Assignment3/1579003.pl
@@ -19,13 +19,12 @@ doOperation('greaterThan', A, B) :- !, A > B.
 doOperation('lessThan', A, B) :- !, A < B.
 doOperation(_, _, _) :- false.
 
-append([], L, L).
-append([A|L1], B, [A|L2]) :- append(L1, B, L2).
-
 filter([], _, _, []).
-filter([A|L], OP, N, [A|L1]) :- number(A), doOperation(OP, A, N), !, filter(L, OP, N, L1).
+filter([A|L], OP, N, [A|L1]) :- number(A), doOperation(OP, A, N), 
+                                !, filter(L, OP, N, L1).
 filter([A|L], OP, N, L1) :- number(A), !, filter(L, OP, N, L1).
-filter([A|L], OP, N, L3) :- filter(A, OP, N, L1), filter(L, OP, N, L2), append(L1, L2, L3).
+filter([A|L], OP, N, L3) :- filter(A, OP, N, L1), filter(L, OP, N, L2), 
+                            append(L1, L2, L3).
 
 % 4
 countOccurence(_, [], 0) :- !. % only one result :)
@@ -34,7 +33,8 @@ countOccurence(X, [_|L], XN) :- countOccurence(X, L, XN).
 
 xCountAll([], _, []).
 xCountAll([X|L], REF, L1) :- isIn(X, L), !, xCountAll(L, REF, L1).
-xCountAll([X|L], REF, [[X,XN]|L1]) :- countOccurence(X, REF, XN), xCountAll(L, REF, L1).
+xCountAll([X|L], REF, [[X,XN]|L1]) :- countOccurence(X, REF, XN), 
+                                      xCountAll(L, REF, L1).
 
 getMinPair([A, AN], [_, BN], [A, AN]) :- AN < BN, !.
 getMinPair(_, X, X).
@@ -54,3 +54,69 @@ pairSort([], []).
 pairSort(L, [MIN|L1]) :- findMinPair(L, MIN, REM), pairSort(REM, L1).
 
 countAll(L, N) :- xCountAll(L, L, N0), pairSort(N0, N).
+
+% 5
+sub([], _, []).
+sub([X|L], S, [X2|L1]) :- atomic(X), !, subReplace(X, S, X2), sub(L, S, L1).
+sub([X|L], S, [L1|L2]) :- sub(X, S, L1), sub(L, S, L2).
+
+subReplace(X, [], X) :- !.
+subReplace(X, [[X,Y]|_], Y) :- !.
+subReplace(X, [_|L], Y) :- subReplace(X, L, Y).
+
+% 6
+clique(L) :- findall(X, node(X), Nodes),
+             subset(L, Nodes), allConnected(L).
+
+subset([], _).
+subset([X|Xs], Set) :- append(_, [X|Set1], Set), subset(Xs, Set1).
+
+allConnected([]).
+allConnected([_|[]]) :- !.
+allConnected([A|L]) :- isConnectedToAll(A, L), allConnected(L).
+
+isConnectedToAll(_, []) :- !.
+isConnectedToAll(A, [B|L]) :- isConnected(A, B), isConnectedToAll(A, L).
+
+isConnected(A, B) :- edge(A, B), !.
+isConnected(A, B) :- edge(B, A).
+
+% todo: delete
+node(a).
+node(b).
+node(c).
+node(d).
+
+edge(a,b).
+edge(b,c).
+edge(c,a).
+edge(c,d).
+edge(a,d).
+
+% 7
+% convert(+Term,-Result)
+convert(Term, Result) :- findAllQ(Term, Borders), xConvert(Term, Borders, Result, 0).
+
+xConvert([], _, [], _).
+xConvert([q|L], B, [q|L1], I) :- !, J is I + 1, xConvert(L, B, L1, J).
+xConvert([X|L], B, [X|L1], I) :- inBorder(I, B), !, J is I + 1, xConvert(L, B, L1, J).
+xConvert([e|L], B, L1, I) :- !, J is I + 1, xConvert(L, B, L1, J).
+xConvert([_|L], B, [c|L1], I) :- J is I + 1, xConvert(L, B, L1, J).
+% q
+% in border
+% out border e
+% out border letter
+
+inBorder(_, []) :- false.
+inBorder(I, [[L, H]|_]) :- L =< I, I =< H, !.
+inBorder(I, [_|B]) :- inBorder(I, B).
+
+findAllQ(L, QL) :- xFindAllQ(L, QL0, 0), pairUp(QL0, QL).
+
+xFindAllQ([], [], _).
+xFindAllQ([q|L], [X|LX], X) :- !, X1 is X + 1, xFindAllQ(L, LX, X1).
+xFindAllQ([_|L], LX, X) :- X1 is X + 1, xFindAllQ(L, LX, X1).
+
+pairUp([], []) :- !.
+pairUp([_|[]], []) :- !.
+pairUp([A, B|L], [[A, B]|L1]) :- pairUp(L, L1).
